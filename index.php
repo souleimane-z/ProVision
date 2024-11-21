50a38566e315279ae5208d8430e57ac8b78d34fa
souleimane-z <souleimane.zeggai@gmail.com>
2024-11-21 12:28:59 +0100

    Header + Banner

--- index.php
+++ index.php
@@ -8,25 +8,25 @@
     <meta name="description" content="">
     <meta name="keywords" content="streaming vidéo, ProVision, plateforme de streaming, apprentissage en ligne, innovation, formation en ligne, vidéos éducatives, contenus interactifs, streaming HD, cours en ligne, vidéos à la demande, e-learning, tutoriels en ligne, innovation technologique, savoir-faire, streaming pédagogique, vidéos professionnelles, contenus exclusifs, plateforme de formation, éducation numérique, skill learning, amélioration des compétences, vision professionnelle, diffusion en continu">
     <meta name="author" content="Souleimane, Hugo, Nassim">
-    <title>ProVision</title>
+    <title>Accueil | ProVision</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
-        <nav>
+        <nav class="header-navbar">
             <img src="#"
                  alt="Logo - ProVision"
                  title="Logo - ProVision"
                  class="header-logo"
             >
-            <ul class="header-nav">
+            <ul class="header-list">
                 <li>
                     <a href="#">Accueil</a>
                 </li>
                 <li>
-                    <a href="#">Films</a>
+                    <a href="pages/moviesShows/movies.php">Films</a>
                 </li>
                 <li>
                     <a href="#">Séries</a>
@@ -37,30 +37,35 @@
             </ul>
 
             <div class="header-search">
-                <label for="searchBar"></label>
-                <input type="text" id="searchBar" placeholder="Rechercher..." required>
-                <button class="btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                <form action="#" method="get">
+                    <label for="searchBar"></label>
+                    <input type="text" id="searchBar" placeholder="Rechercher..." required>
+                    <button class="btn-search"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
             </div>
             <div class="header-auth">
-                <button class="btn-login"><i class="fa-solid fa-right-to-bracket"></i></button>
-                <button class="btn-signup"><i class="fa-solid fa-user-plus"></i></button>
+                <button class="btn-login" title="Se connecter">
+                    <i class="fa-solid fa-right-to-bracket"></i>
+                </button>
+                <button class="btn-signup" title="S'inscrire">
+                    <i class="fa-solid fa-user-plus"></i>
+                </button>
             </div>
         </nav>
         <div class="header-banner">
             <div class="slider">
-            <div class="slide"></div>
-            <div class="slide"></div>
-            <div class="slide"></div>
-            <div class="slide"></div>
+                <div class="slide"></div><div class="slide"></div><div class="slide"></div><div class="slide"></div>
             </div>
             <div>
-
+            <div class="banner-txt">
                 <h1 class="banner-title">ProVision</h1>
 
                 <p class="banner-paragraph">Un monde d'histoires inconnus à la portée d'un clic!</p></div>
-
+            </div>
             </div>
     </header>
+
+    <!-- Icônes sur fontawesome.com -->
     <script src="https://kit.fontawesome.com/386dcd1ba2.js" crossorigin="anonymous"></script>
 </body>
 </html>
