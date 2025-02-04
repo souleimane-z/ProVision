c95aa94fbf58a1ca7f64eda2ec18ff7253bad127
souleimane-z <souleimane.zeggai@gmail.com>
2025-02-04 16:37:22 +0100

    logout icon

--- includes/config.php
+++ includes/config.php
@@ -3,9 +3,7 @@ $logo = "<img src='https://res.cloudinary.com/dhqh98spd/image/upload/v1738224132
 
 /* Bouton : se déconnecter*/
 $isLoggedIn = false;
-$logout_icon = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" class="logout-icon-normal">
-                 <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4ZM7.70711 8.70711L10.5858 11.5858L7.70711 14.4645L9.12132 15.8787L12 13L14.8787 15.8787L16.2929 14.4645L13.4142 11.5858L16.2929 8.70711L14.8787 7.29289L12 10.1716L9.12132 7.29289L7.70711 8.70711Z" fill="#d35d09"/>
-               </svg>';
+$logout_icon = "<img src='https://res.cloudinary.com/dhqh98spd/image/upload/v1738682662/shutdown-svgrepo-com_wgjmzc.png' alt='logout icon' class='logout-icon' title='Se Déconnecter'>";
 
 if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '63342') {
     // Environnement PHPStorm
